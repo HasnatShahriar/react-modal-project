a7cac905db28b2dd321535aea07f183f1dbdf98a
Md. Abu Hasnat Shahriar <108389735+HasnatShahriar@users.noreply.github.com>
2024-09-19 16:14:07 +0600

    fix cards gap and add scrollbar

--- src/utils/helper.jsx
+++ src/utils/helper.jsx
@@ -9,6 +9,10 @@ export const blocksCards = [
   { imgUrl: 'https://i.ibb.co.com/Tgc89Fq/FB-IMG-1624904018722.jpg', isPro: false, category: 'Category 3', title: 'Card 5' },
   { imgUrl: 'https://i.ibb.co.com/4PCfpVt/FB-IMG-1624863412855.jpg', isPro: true, category: 'Category 3', title: 'Card 6' },
   { imgUrl: 'https://i.ibb.co.com/4PCfpVt/FB-IMG-1624863412855.jpg', isPro: false, category: 'Category 1', title: 'Card 7' },
+  { imgUrl: 'https://i.ibb.co.com/4PCfpVt/FB-IMG-1624863412855.jpg', isPro: false, category: 'Category 1', title: 'Card 7' },
+  { imgUrl: 'https://i.ibb.co.com/4PCfpVt/FB-IMG-1624863412855.jpg', isPro: false, category: 'Category 1', title: 'Card 7' },
+  { imgUrl: 'https://i.ibb.co.com/4PCfpVt/FB-IMG-1624863412855.jpg', isPro: false, category: 'Category 1', title: 'Card 7' },
+  { imgUrl: 'https://i.ibb.co.com/4PCfpVt/FB-IMG-1624863412855.jpg', isPro: false, category: 'Category 1', title: 'Card 7' },
 ];
 
 
